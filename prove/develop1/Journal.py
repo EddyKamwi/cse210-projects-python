a2fd038f985b94d7981243bdcc7b7c16ba34605f
Edward Kamwi <edwardkamwi@gmail.com>
2024-07-31 03:28:09 +0200

    commenting my code

--- prove/develop1/Journal.py
+++ prove/develop1/Journal.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 class Journal:
+    # properties
     _date = ''
     _prompt =''
     _promptAns=''
 
+    # constructor
     def __init__(self,prompt,promptAns,date=datetime.now()):
         self._prompt = prompt
         self._promptAns = promptAns
         self._date = date
 
+    # method
     def GetJournalDisplay(self):
         entery = "Date :{} - Prompt:  {}\n{}".format(self._date,self._prompt,self._promptAns)
         return entery
--- prove/develop1/Program.py
+++ prove/develop1/Program.py
@@ -8,7 +8,7 @@ def main():
     CONN = lite.Connection("journal.db")
     CURSOR = CONN.cursor()
 
-
+    # create table if it does not exit
     query =  '''
             CREATE TABLE IF NOT EXISTS entries (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,7 +20,10 @@ def main():
     CURSOR.execute(query)
     CONN.commit()
     
+    # create a dictionary as a virtualfile
     journalEntries = {}
+
+    # all prompts for the program
     prompts = [
             "Who was the most interesting person I interacted with today?",
             "What was the best part of my day?",
@@ -31,10 +34,15 @@ def main():
             "How often did I feel the Savior's love during the day?",
             "What was one thing I did to prepare myself to partake of the sacrament?",
             "Are you happy? Why?"]
+    
+    # program welcome message
     print("Welcome to the Journal Program!")
+
+    # loop main menu
     while True:
         menu = "Please select one of the following choices:\n1. Write\n2. Display\n3. Load \n4. Save \n5.Quit\n> "
         ans = input(menu)
+
         # write to journal
         if ans == "1":
             prompt = r.choice(prompts)
@@ -46,12 +54,23 @@ def main():
             journal = j.Journal(prompt,promptAns)
             date = datetime.now()
             journalEntries[len(journalEntries)] = [prompt,promptAns,date.strftime("%d-%m-%Y")]
+
         # Display to journal
         elif ans=="2":
             for index in journalEntries:
                 pair = journalEntries[index]
                 journal = j.Journal(pair[0],pair[1],pair[2])
                 print(journal.GetJournalDisplay())
+
+        # Load Journal
+        elif ans=="3":
+            query = "SELECT * FROM entries"
+            journalEntries = {}
+            rows = CURSOR.execute(query)
+            CONN.commit()
+            for row in rows:
+                journalEntries[row[0]] = [row[1],row[2],row[3]]       
+
         # Save current Journal
         elif ans=="4":
             if len(journalEntries) !=0:
@@ -64,14 +83,6 @@ def main():
             else:
                 print("Sorry there is nothing to save!")
 
-        # Load Journal
-        elif ans=="3":
-            query = "SELECT * FROM entries"
-            journalEntries = {}
-            rows = CURSOR.execute(query)
-            CONN.commit()
-            for row in rows:
-                journalEntries[row[0]] = [row[1],row[2],row[3]]
         # exit
         elif ans=="5":
             break
